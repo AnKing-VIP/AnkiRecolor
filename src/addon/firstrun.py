57988bbc2be414aa6e708b4e1f381d08505b0dda
Yoonchae Lee <bluegreenmagick@gmail.com>
2022-11-20 17:29:13 +0900

    migrate v1 colors to v2

--- src/addon/firstrun.py
+++ src/addon/firstrun.py
@@ -59,73 +59,92 @@ def compat(prev_version: Version) -> None:
         v1_to_v2()
 
 
+#
+COLOR_MAP = {
+    "TEXT_FG": "FG",
+    "WINDOW_BG": "CANVAS",
+    "FRAME_BG": "CANVAS_INSET",  # Also CANVAS_OVERLAY
+    "BORDER": "BORDER",
+    "MEDIUM_BORDER": "",
+    "FAINT_BORDER": "BORDER_SUBTLE",
+    "LINK": "FG_LINK",
+    "REVIEW_COUNT": "STATE_REVIEW",
+    "NEW_COUNT": "STATE_NEW",
+    "LEARN_COUNT": "STATE_LEARN",
+    "ZERO_COUNT": "FG_FAINT",
+    "SLIGHTLY_GREY_TEXT": "FG_SUBTLE",
+    "HIGHLIGHT_BG": "HIGHLIGHT_BG",
+    "HIGHLIGHT_FG": "HIGHLIGHT_FG",
+    "DISABLED": "FG_DISABLED",
+    "FLAG1_FG": "FLAG_1",
+    "FLAG2_FG": "FLAG_2",
+    "FLAG3_FG": "FLAG_3",
+    "FLAG4_FG": "FLAG_4",
+    "FLAG5_FG": "FLAG_5",
+    "FLAG6_FG": "FLAG_6",
+    "FLAG7_FG": "FLAG_7",
+    "FLAG1_BG": "",
+    "FLAG2_BG": "",
+    "FLAG3_BG": "",
+    "FLAG4_BG": "",
+    "FLAG5_BG": "",
+    "FLAG6_BG": "",
+    "FLAG7_BG": "",
+    "BURIED_FG": "",
+    "SUSPENDED_FG": "STATE_SUSPENDED",
+    "SUSPENDED_BG": "",
+    "MARKED_BG": "STATE_MARKED",
+    "TOOLTIP_BG": "CANVAS_OVERLAY",
+    "BUTTON_BG": "BUTTON_BG",
+    "CURRENT_DECK": "",
+}
+
+
 # To Anki 2.1.55+ theme style
 def v1_to_v2() -> None:
     conf.load()
 
-    # Save v1 colors
+    # Load and save v1 colors
     user_files_dir = Path(__file__).parent / "user_files"
     user_files_dir.mkdir(parents=False, exist_ok=True)
-    v1_colors_path = user_files_dir / "v1_colors.json"
-    with v1_colors_path.open("w") as f:
-        v1_colors = conf.get("colors")
-        json.dump(v1_colors, f, indent=2)
-    # Load v2 colors
+    v1_colors_path = user_files_dir / "v1_config.json"
+    v1_colors_path.write_text(json.dumps(conf.get(""), indent=2))
+    v1_colors = conf.get("colors")
+    v1_anki_colors_path = Path(__file__).parent / "v1_anki_config.json"
+    v1_anki_colors = json.loads(v1_anki_colors_path.read_text())["colors"]
+
+    # Load v2 vinally anki colors
+    v2_colors = conf.get_default("colors")
     new_colors = conf.get_default("colors")
+
+    # old -> new for colors not identical to vanilla anki
+    for old_name in COLOR_MAP:
+        new_name = COLOR_MAP[old_name]
+        if new_name:
+            if v1_colors[old_name][1] != v1_anki_colors[old_name][1]:
+                new_colors[new_name][1] = v1_colors[old_name][1]
+            if v1_colors[old_name][2] != v1_anki_colors[old_name][2]:
+                new_colors[new_name][2] = v1_colors[old_name][2]
+
+    if new_colors["CANVAS"][1] != v2_colors["CANVAS"][1]:
+        new_colors["CANVAS_OVERLAY"][1] = new_colors["CANVAS"][1]
+    if new_colors["CANVAS"][2] != v2_colors["CANVAS"][2]:
+        new_colors["CANVAS_OVERLAY"][2] = new_colors["CANVAS"][2]
+
     conf.set("colors", new_colors)
     conf.save()
+
     # Inform users that previous ReColor config is no longer valid
     showInfo(
         "\n".join(
             [
                 "Anki v2.1.55+ has reworked the themes code, so the previous ReColor config no longer works with the new themes.",
-                "ReColor config has been reset to work with the new Anki themes, and you will need to reconfigure ReColor.",
-                "The previous ReColor config have been saved to the addon user_files directory, 'v1_colors.json'.",
+                "Your previous ReColor theme has been moved over to the new config as best as possible, but things may look different.",
+                "Your previous ReColor config have been saved to the addon user_files directory, 'v1_config.json'.",
             ]
         ),
         title="Anki Recolor Update",
     )
-    """
-    # old -> new
-    color_map = {
-        "TEXT_FG": "FG",
-        "WINDOW_BG": "CANVAS",
-        "FRAME_BG": "",
-        "BORDER": "BORDER",
-        "MEDIUM_BORDER": "",
-        "FAINT_BORDER": "",
-        "LINK": "FG_LINK",
-        "REVIEW_COUNT": "",
-        "NEW_COUNT": "",
-        "LEARN_COUNT": "",
-        "ZERO_COUNT": "",
-        "SLIGHTLY_GREY_TEXT": "",
-        "HIGHLIGHT_BG": "HIGHLIGHT_BG",
-        "HIGHLIGHT_FG": "HIGHLIGHT_FG",
-        "DISABLED": "",
-        "FLAG1_FG": "",
-        "FLAG2_FG": "",
-        "FLAG3_FG": "",
-        "FLAG4_FG": "",
-        "FLAG5_FG": "",
-        "FLAG6_FG": "",
-        "FLAG7_FG": "",
-        "FLAG1_BG": "",
-        "FLAG2_BG": "",
-        "FLAG3_BG": "",
-        "FLAG4_BG": "",
-        "FLAG5_BG": "",
-        "FLAG6_BG": "",
-        "FLAG7_BG": "",
-        "BURIED_FG": "",
-        "SUSPENDED_FG": "",
-        "SUSPENDED_BG": "",
-        "MARKED_BG": "",
-        "TOOLTIP_BG": "",
-        "BUTTON_BG": "",
-        "CURRENT_DECK": "",
-    }
-    """
 
 
 compat(prev_version)
