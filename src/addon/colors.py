f35288cde4e8965c1d12207e8000cd7c917b848d
Yoonchae Lee <bluegreenmagick@gmail.com>
2022-11-20 13:09:32 +0900

    cleanup theme change code. Only works with 2.1.55+ https://github.com/ankitects/anki/pull/2199 needs to be merged for this to work

--- src/addon/colors.py
+++ src/addon/colors.py
@@ -1,46 +1,18 @@
 from typing import Any, Optional
 
-from anki.utils import isWin, isMac, pointVersion
-
 import aqt
 from aqt.webview import AnkiWebView
 import aqt.colors
-from aqt import gui_hooks, mw, dialogs
+from aqt import gui_hooks, mw
 from aqt.theme import theme_manager
-from aqt.qt import QColor, QPalette, Qt
+from aqt.qt import QColor
 
 from .ankiaddonconfig import ConfigManager
 
-ankiver_minor = pointVersion()
 conf = ConfigManager()
 
 
 # ReColor Python Colors
-
-
-def refresh_all_windows() -> None:
-    # Redraw top toolbar
-    mw.toolbar.draw()
-    # Redraw main body
-    if mw.state == "review":
-        mw.reviewer._initWeb()
-        if mw.reviewer.state == "question":
-            mw.reviewer._showQuestion()
-        else:
-            mw.reviewer._showAnswer()
-    elif mw.state == "overview":
-        mw.overview.refresh()
-    elif mw.state == "deckBrowser":
-        mw.deckBrowser.refresh()
-    # Close Browser if open
-    browser = dialogs._dialogs["Browser"][1]
-    if browser:
-        browser.closeWithCallback(lambda: None)
-    if ankiver_minor >= 45:  # has mw.flags
-        if mw.col is not None:
-            mw.flags._load_flags()
-
-
 def recolor_python() -> None:
     conf.load()
     color_entries = conf.get("colors")
@@ -51,152 +23,35 @@ def recolor_python() -> None:
             anki_color["dark"] = color_entry[2]
             setattr(aqt.colors, color_name, anki_color)
     theme_manager.apply_style()
-    refresh_all_windows()
-
-
-def qcolor(conf_key: str) -> QColor:
-    color_idx = 2 if theme_manager.night_mode else 1
-    hex_color = conf.get(f"colors.{conf_key}.{color_idx}")
-    return QColor(hex_color)
-
-
-def apply_palette() -> None:
-    # theme_manager._apply_palette() can only be run in night mode
-    color_map = {
-        QPalette.ColorRole.WindowText: "TEXT_FG",
-        QPalette.ColorRole.ToolTipText: "TEXT_FG",
-        QPalette.ColorRole.Text: "TEXT_FG",
-        QPalette.ColorRole.ButtonText: "TEXT_FG",
-        QPalette.ColorRole.HighlightedText: "HIGHLIGHT_FG",
-        QPalette.ColorRole.Window: "WINDOW_BG",
-        QPalette.ColorRole.AlternateBase: "WINDOW_BG",
-        QPalette.ColorRole.Button: "BUTTON_BG",
-        QPalette.ColorRole.Base: "FRAME_BG",
-        QPalette.ColorRole.ToolTipBase: "FRAME_BG",
-        QPalette.ColorRole.Link: "LINK",
-    }
-    disabled_roles = [
-        QPalette.ColorRole.Text,
-        QPalette.ColorRole.ButtonText,
-        QPalette.ColorRole.HighlightedText,
-    ]
-
-    palette = QPalette()
-
-    for role in color_map:
-        conf_key = color_map[role]
-        palette.setColor(role, qcolor(conf_key))
-
-    hlbg = qcolor("HIGHLIGHT_BG")
-    if theme_manager.night_mode:
-        hlbg.setAlpha(64)
-    palette.setColor(QPalette.ColorRole.Highlight, hlbg)
-
-    for role in disabled_roles:
-        palette.setColor(QPalette.ColorGroup.Disabled, role, qcolor("DISABLED"))
-
-    if theme_manager.night_mode:
-        palette.setColor(QPalette.ColorRole.BrightText, Qt.GlobalColor.red)
-
-    mw.app.setPalette(palette)
-    # webview palette uses default one
-    theme_manager.default_palette = palette  # type: ignore
 
 
 def get_window_bg_color(*args: Any) -> QColor:
     color_idx = 2 if theme_manager.night_mode else 1
-    hex_color = conf.get(f"colors.WINDOW_BG.{color_idx}")
+    hex_color = conf.get(f"colors.CANVAS.{color_idx}")
     return QColor(hex_color)
 
 
 def replace_webview_bg() -> None:
-    if 26 <= ankiver_minor <= 44:
-        AnkiWebView._getWindowColor = get_window_bg_color  # type: ignore
-    elif 45 <= ankiver_minor:
-        AnkiWebView.get_window_bg_color = get_window_bg_color  # type: ignore
+    AnkiWebView.get_window_bg_color = get_window_bg_color  # type: ignore
 
 
 # ReColor CSS Colors
 
 
-def file_url(file_name: str) -> str:
-    addon_package = mw.addonManager.addonFromModule(__name__)
-    return f"/_addons/{addon_package}/{file_name}"
-
-
-def wrap_style(code: str) -> str:
-    return f"<style>{code}</style>"
-
-
 def inject_web(web_content: aqt.webview.WebContent, context: Optional[Any]) -> None:
     conf.load()
-    night_mode = theme_manager.night_mode
-    color_idx = 2 if night_mode else 1
-    web_content.body += "<script>ReColor.withConfig('{}', {})</script>".format(
-        conf.to_json(), color_idx
-    )
-    web_content.body += (
-        "<style>.current{ background-color: var(--current-deck); }</style>"
-    )
-    web_content.js.append(file_url("recolor.js"))
+    colors_config = conf["colors"]
 
-    # Override night mode button color
-    btn_bg_night = conf.get("colors.BUTTON_BG.2")
-    extra_style = ""
-    if night_mode:  # Created to look good enough
-        if conf.get_default("colors.BUTTON_BG.2") != btn_bg_night:
-            extra_style += """
-                .night_mode button {
-                    background: %(btn_bg)s;
-                    border: none; 
-                }
-                .night_mode button:hover {
-                    background: %(btn_bg)s;
-                    filter: brightness(1.25);
-                }
-            """ % {
-                "btn_bg": btn_bg_night
-            }
-    else:  # Because v2.1.44- uses QPalette default color
-        if isWin:
-            extra_style += """
-                button:focus {
-                    outline: 1px solid %s
-                }
-            """ % conf.get(
-                "colors.HIGHLIGHT_BG.1"
-            )
-        elif isMac:
-            extra_style += """
-                button {
-                    background: %(btn_bg)s;
-                }
-            """ % {
-                "btn_bg": conf.get("colors.BUTTON_BG.1")
-            }
-        else:
-            extra_style += """
-                    button {
-                        background-color: %(btn_bg)s;
-                    }
-                    button:active, button:active:hover { 
-                        background-color: %(color_hl)s; 
-                        color: %(color_hl_txt)s;
-                    }
-                    btn:focus { border-color: %(color_hl)s }
-                    textarea:focus, input:focus, input[type]:focus, 
-                    .uneditable-input:focus, div[contenteditable="true"]:focus {   
-                        border-color: %(color_hl)s;
-                    }
-            """ % {
-                "btn_bg": conf.get("colors.BUTTON_BG.1"),
-                "color_hl": conf.get("colors.HIGHLIGHT_BG.1"),
-                "color_hl_txt": conf.get("colors.HIGHLIGHT_FG.1"),
-            }
+    light_mode_css = ""
+    dark_mode_css = ""
+    for name in colors_config:
+        entry = colors_config[name]
+        css_name = entry[3]
+        light_mode_css += f"{css_name}: {entry[1]};\n"
+        dark_mode_css += f"{css_name}: {entry[2]};\n"
 
-    web_content.body += wrap_style(extra_style)
+    web_content.head += f"<style>body {{ \n{light_mode_css} }}\nbody.night_mode {{ \n{dark_mode_css} }}</style>"
 
 
-mw.addonManager.setWebExports(__name__, "recolor.js")
 gui_hooks.webview_will_set_content.append(inject_web)
 recolor_python()
